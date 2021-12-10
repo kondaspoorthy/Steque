603e8bfec2c911948af4da77c04f589e7112fd2f
Kondaspoorthy <kondasaispoorthy@msitprogram.net>
2021-12-10 13:54:51 +0530

    empty function using Linkedlist

--- Steque.java
+++ Steque.java
@@ -111,6 +111,7 @@ public class Steque<Item> implements Iterable<Item> {
      * @return true if steque is empty, false otherwise.
      */
     public boolean isEmpty() {
+        return first==null;
         
 
     }
